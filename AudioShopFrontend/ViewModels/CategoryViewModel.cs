b6289137172ef5d38e3e332b018405cf8159072f
RajabiA <RajabiA@RAJABIA>
2021-01-07 17:37:36 +0330

    category page navigation started

---
+++ AudioShopFrontend/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AudioShopFrontend.Models;
+
+namespace AudioShopFrontend.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public List<Category_Brands> CategoryBrands { get; set; }
+        public List<Category_Types> CategoryTypes { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
